d6b787bf9fb3ff4c9211722873bd1648db512da2
Frankie Primerano <frankieprimerano@gmail.com>
2016-07-24 16:29:18 -0400

    Added setup.py

---
+++ coffer/__init__.py
@@ -0,0 +1,8 @@
+import utils
+import clone
+import coffer
+import create
+import enter
+import list
+import remove
+import version
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import *
+
+kwargs = {
+	"author" : "Frankie Primerano",
+	"author_email" : "frankieprimerano@gmail.com",
+	"description" : "A lightweight filesystem container for creating isolated development environments.",
+	"entry_points" : {"console_scripts" : ["coffer=coffer.coffer:checkArgs"]},
+	"name" : "Coffer",
+	"packages" : ["coffer", "coffer.utils"],
+	"version" : "1.0.0",
+}
+
+setup(**kwargs)
