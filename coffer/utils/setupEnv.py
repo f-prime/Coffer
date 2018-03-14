465bdc7a76d4145dcf6eec620fb7c654a91de164
Frankie <frankieprimerano@gmail.com>
2018-03-14 12:34:45 -0400

    Fixed debootstrap

--- coffer/utils/setupEnv.py
+++ coffer/utils/setupEnv.py
@@ -15,12 +15,14 @@ def setup():
     with open(path, 'wb') as tarf:
         tarf.write(deboot)
     tarfile.open(path).extractall(path=root)
+    os.rename(root + "debootstrap-1.0.92~bpo9+1", root + "debootstrap") 
     os.remove(path)
     edit = open(root + "debootstrap/debootstrap").readlines()
+    edit[485] = "#" + edit[485]
+    edit[486] = "#" + edit[486]
     edit[487] = "#" + edit[487]
-    edit[488] = "#" + edit[488]
-    edit[489] = "#" + edit[489]
     edit = "".join(edit)
     edit = "DEBOOTSTRAP_DIR={}\n".format(root + "debootstrap") + edit
     with open(root + "debootstrap/debootstrap", 'w') as w:
         w.write(edit) 
+
