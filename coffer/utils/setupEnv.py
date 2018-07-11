443941f92a53afdef09b0b3f846770e5606d0779
Frankie <frankieprimerano@gmail.com>
2018-07-11 01:36:58 -0400

    Added debootstrap as dep within repository

--- coffer/utils/setupEnv.py
+++ coffer/utils/setupEnv.py
@@ -10,17 +10,17 @@ def setup():
     os.mkdir(root)
     os.mkdir(envs)
     print (text.downloadingFiles)
-    deboot = urllib.urlopen("http://ftp.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.92~bpo9+1.tar.gz").read()
+    deboot = urllib.urlopen("https://github.com/f-prime/Coffer/tree/master/coffer/deps/debootstrap.tar.gz").read()
     path = root + "deboot.tar"
     with open(path, 'wb') as tarf:
         tarf.write(deboot)
     tarfile.open(path).extractall(path=root)
-    os.rename(root + "debootstrap-1.0.92~bpo9+1", root + "debootstrap") 
+    os.rename(root + "debootstrap-1.0.106", root + "debootstrap") 
     os.remove(path)
     edit = open(root + "debootstrap/debootstrap").readlines()
-    edit[485] = "#" + edit[485]
-    edit[486] = "#" + edit[486]
-    edit[487] = "#" + edit[487]
+    edit[516] = "#" + edit[485]
+    edit[517] = "#" + edit[486]
+    edit[518] = "#" + edit[487]
     edit = "".join(edit)
     edit = "DEBOOTSTRAP_DIR={}\n".format(root + "debootstrap") + edit
     with open(root + "debootstrap/debootstrap", 'w') as w:
